811dcb0430e3ccf797644fd680e5980bc1439e63
JIVitian <jivitian@gmail.com>
2021-10-26 19:18:30 -0300

    Probando Bulma con react

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
-import Bootstrap from "./components/bootstrap";
+import Bootstrap from "./components/Bootstrap";
+import Bulma from "./components/Bulma";
 
 function App() {
   return (
     <div className="App">
       <h1>Librerias CSS con React</h1>
       <Bootstrap />
+      <Bulma />
     </div>
   );
 }
